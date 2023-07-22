0a981fb3b80692c3d95d48b6a27e4ef99548462f
dips98 <upadhayay.dipesh@gmail.com>
2023-07-23 01:41:05 +0530

    Async call

--- FileSystem.js
+++ FileSystem.js
@@ -1,3 +1,12 @@
 const fs = require('fs');
 
 console.log(fs.readdirSync('./'));
+
+fs.readdir('./',function(err,files){
+    if(err){
+        console.log("Error :",err);
+    }else{
+        console.log("Files :",files)
+    }
+
+});
