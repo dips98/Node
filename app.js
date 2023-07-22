845ab48cf653cf5aae06320dd0e9ef012ea657a1
dips98 <upadhayay.dipesh@gmail.com>
2023-07-23 00:20:26 +0530

    Used to call with module

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var logger = require("./logger");
-function sayHello(name){
-  console.log("Hello",name);
-}
-var message = '';
-sayHello("Dipesh");
-console.log(module);
-console.log(global);
-console.log(global.message); //It will get undefined
+// function sayHello(name){
+//   console.log("Hello",name);
+// }
+// var message = '';
+// sayHello("Dipesh");
+// console.log(module);
+// console.log(global);
+// console.log(global.message); //It will get undefined
+
+console.log(logger);
+logger.log("Its working fine dude!");
