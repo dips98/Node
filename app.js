3cb8b3f7dbfd0df3b40dda5598441f20d9bb8bab
dips98 <upadhayay.dipesh@gmail.com>
2023-07-26 23:42:22 +0530

    No need of Event emmiter object

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const Logger = require("./logger");
-const EventEmmiters = require('events');
-const emmiter = new EventEmmiters();
 
 const logger = new Logger();
 logger.on('messagelogged',(arg)=>{
